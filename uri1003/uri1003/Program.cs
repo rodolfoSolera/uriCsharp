95e3e1356ecd87ece686362f1fe900a135749767
Rodolfo Solera <rodolfo.velho@outlook.com>
2024-08-25 22:41:38 -0300

    #uri1003 - Resolução Exercício 1003

---
+++ uri1003/uri1003/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class uri1003
+{
+
+    static void Main(string[] args)
+    {
+
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        int SOMA = A + B;
+
+        Console.WriteLine("SOMA = " + SOMA);
+
+    }
+
+}
