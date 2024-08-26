f71e823b53599f695aa9b4523006c2f63ab3420d
Rodolfo Solera <rodolfo.velho@outlook.com>
2024-08-25 22:57:35 -0300

    #uri1004 - Inclusão de Exercício 1004

---
+++ uri1004/uri1004/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class uri1004
+{
+
+    static void Main(string[] args)
+    {
+
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+
+        int PROD = n1 * n2;
+
+        Console.WriteLine("PROD = " + PROD);
+    }
+
+}
