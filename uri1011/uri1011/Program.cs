29986c0fd3bfd055af0599cfe89ceaaaf806a4c6
Rodolfo Solera <rodolfo.velho@outlook.com>
2024-08-26 23:23:11 -0300

    #uri1011 - Resolução Exercício 1011

---
+++ uri1011/uri1011/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class uri1011
+{
+
+    static void Main(string[] args)
+    {
+
+        int x = 60;
+        int y = 90;
+
+        int km = int.Parse(Console.ReadLine());
+
+        int minutos = km * 2;
+
+        Console.WriteLine(minutos + " minutos");
+
+    }
+
+}
